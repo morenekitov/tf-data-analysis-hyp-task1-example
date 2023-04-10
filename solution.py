f8fefd7e6b3dd836cc331db3b252e6f342d86ba7
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-10 19:01:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 287133833 
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    a_val = 0.09
+    # h0 - средние одинаковы
+
+    loc_suc_x, loc_suc_y = x_success / x_cnt, y_success / y_cnt
+    var_suc_x,var_suc_y = (x_success / x_cnt)*((x_cnt - x_success) / x_cnt), (y_success / y_cnt)*((y_cnt - y_success) / y_cnt)
+    se_loc_suc_x,se_loc_suc_y = np.sqrt(var_suc_x) / np.sqrt(x_cnt), np.sqrt(var_suc_y) / np.sqrt(y_cnt)
+    P = (y_success+x_success)/ (x_cnt + y_cnt)
+    z =(loc_suc_x-loc_suc_y) / np.sqrt(   P*(1-P) * (1/x_cnt + 1/y_cnt))
+    threshold = norm.ppf(1-a_val, loc=0, scale=1) 
+    return (1 if z > threshold else 0)
